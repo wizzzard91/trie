742208dcaa9f99128f3ccd86ddf58af4b2c1c471
Bulat Nabiullin <bulat.nabiullin@onetwotrip.com>
2019-12-23 18:01:33 +0300

    Made package from trie

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='trie',
+      version='1.0',
+      description='Trie',
+      classifiers=[
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Text Processing',
+      ],
+      keywords='trie',
+      url='https://github.com/wizzzard91/trie',
+      author='Bulat Nabiullin',
+      author_email='bulat.nabiullin@onetwotrip.com',
+      packages=find_packages(),
+      install_requires=[],
+      include_package_data=True,
+      zip_safe=False)
